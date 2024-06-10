1884d38598a8a64ab03ca8991e07f12ddbe29cfa
SushantGanji3 <69330961+SushantGanji3@users.noreply.github.com>
2024-06-10 18:30:23 +0000

    1st draft

--- linkedin/02_01/insertcustomerrecords.sql
+++ linkedin/02_01/insertcustomerrecords.sql
@@ -2,5 +2,8 @@ INSERT INTO customers (firstname, lastname, username, password, email, created_o
 VALUES ('Alison', 'Riesher', 'alisoninthesun', 'l09+#k$nalA!', 'sunnysideup@myemail.com', CURRENT_TIMESTAMP), 
 ('Edgar', 'Tobin', 'edgar163', 'BSI@uj0_qwT%', 'edgar163@email.com', CURRENT_TIMESTAMP),
 ('Frank', 'Lawson', 'frank_lawson', '32oktn*_WKn89', 'frank@lawsonhardware.com', CURRENT_TIMESTAMP),
-('Kiara', 'Mendez', 'kiaradiamond', '9s9dSn$LStt', 'kdiamond@myemail.com', CURRENT_TIMESTAMP),
+('Kiara', 'Mendez', 'kiardiamond', '9s9dSn$LStt', 'kdimond@myemail.com', CURRENT_TIMESTAMP),
+('Kiaaa', 'Mendez', 'kiaadiamond', '9s9dSn$LStt', 'kiamond@myemail.com', CURRENT_TIMESTAMP),
+('Kiarq', 'Mendez', 'kiradiamond', '9s9dSn$LStt', 'kdamond@myemail.com', CURRENT_TIMESTAMP),
+('Kqara', 'Mendez', 'kiaradiamond', '9s9dSn$LStt', 'kdiamond@myemail.com', CURRENT_TIMESTAMP),
 ('Taylor', 'Hiu', 'taytay89', 'U09$hjqlaBM', 'taytay89@myemail.com', CURRENT_TIMESTAMP);
